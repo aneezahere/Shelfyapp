6b9478eb506c3dbee7321e39b97b4b0ad8f5838d
Khansa <khansa@Khansas-MacBook-Air.local>
2024-08-09 02:44:14 -0400

    CODE UPDATED

--- firebase.js
+++ firebase.js
@@ -1,21 +1,26 @@
+// firebase.js
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "pantrytracking.firebaseapp.com",
-  projectId: "pantrytracking",
-  storageBucket: "pantrytracking.appspot.com",
-  messagingSenderId: "57108758196",
-  appId: "1:57108758196:web:d0c233b5d7073a650e21e1",
-  measurementId: "G-QVTEGE1PD0"
-};
-
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Analytics only if window is defined (client-side)
+let analytics;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
+
+export { app, analytics };
