c2737f70807dc4c406696699797b4544cbb2cf44
Khansa <khansa@Khansas-MacBook-Air.local>
2024-08-09 01:34:35 -0400

    Removed API key and replaced with environment variable

--- firebase.js
+++ firebase.js
@@ -1,17 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBMZ6fwKTxfoiXl4-v_avLeo93StHLXJAk",
-    authDomain: "pantrytracking.firebaseapp.com",
-    projectId: "pantrytracking",
-    storageBucket: "pantrytracking.appspot.com",
-    messagingSenderId: "57108758196",
-    appId: "1:57108758196:web:d0c233b5d7073a650e21e1",
-    measurementId: "G-QVTEGE1PD0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "pantrytracking.firebaseapp.com",
+  projectId: "pantrytracking",
+  storageBucket: "pantrytracking.appspot.com",
+  messagingSenderId: "57108758196",
+  appId: "1:57108758196:web:d0c233b5d7073a650e21e1",
+  measurementId: "G-QVTEGE1PD0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
